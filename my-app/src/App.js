46fd122912e2ae9068f7dc122c04dc1fb5054e07
Ahmad <ahmedhassan.26444@gmail.com>
2025-05-11 16:11:48 +0500

    initial

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+function MyComponet() {
+  return (
+    <div>
+    componet
+    </div>
+  );
+}
+function App() {
+  return (
+    <div>
+    hello <MyComponet/> guys 
+    </div>
+  );
+}
+export default App;
