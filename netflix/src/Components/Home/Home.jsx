580c9ee7a82d80e2f1dee04d742bd7a439d2a961
Ahmad <ahmedhassan.26444@gmail.com>
2025-05-24 23:12:57 +0500

    lecture 7 (netflix clone project minor changes )

--- netflix/src/Components/Home/Home.jsx
+++ netflix/src/Components/Home/Home.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import './home.scss'
-const home = () => {
+import React from 'react';
+import './home.scss';
+
+const Home = () => {
   return (
-    <div>
-      Home
-    </div>
+    <section className="home">
+      <div className="banner"></div>
+    </section>
   )
-}
-export default home
+};
+
+export default Home;
