a2ef9210d70a6fe596c01f50627f299249ce3c13
Ahmad <ahmedhassan.26444@gmail.com>
2025-06-02 19:52:22 +0500

    lecture 9 ( chatapp minor changes )

--- chatapp/src/App.js
+++ chatapp/src/App.js
@@ -1,11 +1,29 @@
-import { Box, Button, Container, VStack } from "@chakra-ui/react";
+import { Box, Button, Container, VStack, Input, HStack } from "@chakra-ui/react";
 
 function App() {
   return (
     <Box bg="red.50">
       <Container h="100vh" bg="white">
-        <VStack>
-          <Button>Sign out</Button>
+        <VStack h="full" bg="telegram.100" justifyContent="space-between">
+          <Button colorScheme="red" w="full">
+            Logout
+          </Button>
+
+          <VStack
+            w="full"
+            h="full"
+            bg="purple.100"
+            flexGrow={1}
+            justifyContent="flex-end"
+            p={4}
+          >
+            <HStack as="form" w="full">
+              <Input flex={1} placeholder="Type your message..." />
+              <Button colorScheme="purple" type="submit">
+                Send
+              </Button>
+            </HStack>
+          </VStack>
         </VStack>
       </Container>
     </Box>
