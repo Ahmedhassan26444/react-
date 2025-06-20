dfe71e71875a83ff38e472e3c871d3c74a346c44
Ahmad <ahmedhassan.26444@gmail.com>
2025-06-20 23:12:51 +0500

    lecture 10 ( class base component minor changes
    )

--- class-base-component/src/App.js
+++ class-base-component/src/App.js
@@ -4,18 +4,21 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      count: 0,
-      name: "Guest"
+      count: 0
     };
   }
+
+  increment = () => {
+    this.setState((prevState) => ({
+      count: prevState.count + 1
+    }));
+  };
 
   render() {
     return (
       <div className="App">
-        <p>
-          {this.state.name}
-        </p>
-        <p>Count: {this.state.count}</p> {/* Example of using count */}
+        <p>{this.state.count}</p>   {/* Count on top */}
+        <button onClick={this.increment}>Increment</button>  {/* Button below */}
       </div>
     );
   }
