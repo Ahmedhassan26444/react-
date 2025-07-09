4c6e493012ca0ec74c1961cfcc86f2193922f533
Ahmedhassan26444 <ahmedhassan.26444@gmail.com>
2025-07-09 22:06:48 +0500

    lecture-12( chaiwala-clone-project minor changes )

--- chaiwala-clone/src/App.js
+++ chaiwala-clone/src/App.js
@@ -2,12 +2,19 @@ import "./Style/App.scss";
 import "./Style/intro.scss";
 import "./Style/section.scss";
 import Introvedio from "./Components/introvedio";
+import FreshTopicImg from "./Assets/academy.png"; // ✅ FIX: Backslash ➝ Forward slash
 
 function App() {
   return (
     <div className="App">
       <Introvedio />
-      <section />
+      {/* FreshTopic */}
+      <div className="section">
+        <h3>asdfrex</h3>
+        <p>asdfcc</p>
+        <button>asdfff</button>
+        <img src={FreshTopicImg} alt="Fresh Topic" />
+      </div>
     </div>
   );
 }
