c22b5f691d3eb2d72366ebadf70ad50ccfea61c2
Ahmad Hassan <ahmedhassan.26444@gmail.com>
2025-07-31 08:40:43 -0700

    lecture-12( chaiwala-clone-project minor changes )

---
+++ chaiwala-clone/src/Components/footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <h1>
+        Let's
+        <br />
+        #ConnectOnCutting?
+      </h1>
+      <aside>
+
+      </aside>
+    </footer>
+  );
+};
+
+export default Footer;
