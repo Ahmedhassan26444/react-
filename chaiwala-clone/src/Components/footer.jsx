cf31b59db9042e3ac0bf9fcaf22d074440f56c95
Ahmad Hassan <ahmedhassan.26444@gmail.com>
2025-08-01 11:57:54 -0700

    lecture-12( chaiwala-clone-project minor changes )

--- chaiwala-clone/src/Components/footer.jsx
+++ chaiwala-clone/src/Components/footer.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
+    <>
     <footer>
       <h1>
         Let's
@@ -9,9 +10,41 @@ const Footer = () => {
         #ConnectOnCutting?
       </h1>
       <aside>
+<a href="/" className="footerlinkc">
+home
+</a>
+<a href="/story" className="footerlinkc">
+story
+</a>
+<a href="/media" className="footerlinkc">
+media
+</a>
+<a href="/franchise" className="footerlinkc">
+franchise
+</a>
+<a href="/events" className="footerlinkc">
+    Events 
+</a>
+<a href="/chaiwalacares" className="footerlinkc">
+    chai wala cares
+</a>
+</aside>
+<div>
+  <h5>Email</h5>
+  <a href="mailto:info@mbachaiwala.com">Info@mbachaiwala.com</a><br />
+  <a href="mailto:franchise@mbachaiwala.com">Franchise@mbachaiwala.com</a>
+</div>
 
-      </aside>
-    </footer>
+<div>
+  <h5>Phone</h5>
+  <a href="tel:+91722905222">+91722905222</a>
+</div>
+<p>
+  ©CopyRight 2022 MB CHAIWALA. MADE BY 
+</p>
+</footer>
+<div className="footer"></div>
+</>
   );
 };
 
