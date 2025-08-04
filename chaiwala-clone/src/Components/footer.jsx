2525452987ce44f503851dc8b6aa289d05df8a99
Ahmad Hassan <ahmedhassan.26444@gmail.com>
2025-08-04 09:37:36 -0700

    lecture-12( chaiwala-clone-project minor changes )

--- chaiwala-clone/src/Components/footer.jsx
+++ chaiwala-clone/src/Components/footer.jsx
@@ -1,50 +1,39 @@
 import React from "react";
-
 const Footer = () => {
   return (
     <>
-    <footer>
-      <h1>
-        Let's
-        <br />
-        #ConnectOnCutting?
-      </h1>
-      <aside>
-<a href="/" className="footerlinkc">
-home
-</a>
-<a href="/story" className="footerlinkc">
-story
-</a>
-<a href="/media" className="footerlinkc">
-media
-</a>
-<a href="/franchise" className="footerlinkc">
-franchise
-</a>
-<a href="/events" className="footerlinkc">
-    Events 
-</a>
-<a href="/chaiwalacares" className="footerlinkc">
-    chai wala cares
-</a>
-</aside>
-<div>
-  <h5>Email</h5>
-  <a href="mailto:info@mbachaiwala.com">Info@mbachaiwala.com</a><br />
-  <a href="mailto:franchise@mbachaiwala.com">Franchise@mbachaiwala.com</a>
-</div>
+      <footer>
+        <h1>
+          Let's
+          <br />
+          #ConnectOnCutting?
+        </h1>
+        <aside>
+          <a href="/" className="footerlink">home</a>
+          <a href="/story" className="footerlink">story</a>
+          <a href="/media" className="footerlink">media</a>
+          <a href="/franchise" className="footerlink">franchise</a>
+          <a href="/events" className="footerlink">Events</a>
+          <a href="/chaiwalacares" className="footerlink">chai wala cares</a>
+        </aside>
 
-<div>
-  <h5>Phone</h5>
-  <a href="tel:+91722905222">+91722905222</a>
-</div>
-<p>
-  ©CopyRight 2022 MB CHAIWALA. MADE BY 
-</p>
-</footer>
-<div className="footer"></div>
-</>
+        <div>
+          <h5>Email</h5>
+          <a href="mailto:info@mbachaiwala.com">Info@mbachaiwala.com</a><br />
+          <a href="mailto:franchise@mbachaiwala.com">Franchise@mbachaiwala.com</a>
+        </div>
+
+        <div>
+          <h5>Phone</h5>
+          <a href="tel:+91722905222">+91722905222</a>
+        </div>
+
+        <p>
+          ©CopyRight 2022 MB CHAIWALA. MADE BY 
+        </p>
+      </footer>
+      <div className="footer"></div>
+    </>
   );
 };
 
