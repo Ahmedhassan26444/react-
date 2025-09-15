11bf1610bbd7839dc94f67adcb0e59d8a25d59ed
Ahmad Hassan <ahmedhassan.26444@gmail.com>
2025-09-15 08:18:59 -0700

    lecture 15 ( project-cyprto-app )

---
+++ cyrpto-app/src/App.js
@@ -0,0 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import CoinDetials from "./components/CoinDetials";
+import Coins from "./components/Coins";
+import Exchanges from "./components/Exchanges";
+import Home from "./components/Home";
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/coins" element={<Coins />} />
+        <Route path="/exchanges" element={<Exchanges />} />
+        <Route path="/coins/:id" element={<CoinDetials />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ cyrpto-app/src/components/CoinDetials.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CoinDetials = () => {
+  return (
+    <div>CoinDetials</div>
+  )
+}
+
+export default CoinDetials
