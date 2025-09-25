6b20846588972e8a7e3ac64f76832f771aa57b36
Ahmad Hassan <ahmedhassan.26444@gmail.com>
2025-09-25 01:10:38 -0700

    lecture 17 ( project-react-cart with minor changes )

--- react-card/src/App.js
+++ react-card/src/App.js
@@ -1,7 +1,10 @@
-import "./styles/colors.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Header from "./components/Header";
+import { Toaster } from "react-hot-toast";
+import Cart from "./components/Cart";
+
+import "./styles/app.scss";
 
 function App() {
   return (
@@ -9,7 +12,9 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
+      <Toaster />
     </BrowserRouter>
   );
 }
--- react-card/src/components/Header.jsx
+++ react-card/src/components/Header.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { FiShoppingBag } from "react-icons/fi";
+import { useSelector } from "react-redux";
 
 const Header = () => {
+  const { cartItems } = useSelector((state) => state.cart);
   return (
-    <div>Header</div>
-  )
-}
+    <nav>
+      <h2>Logo Here.</h2>
 
-export default Header
+      <div>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/cart"}>
+          <FiShoppingBag />
+          <p>{cartItems.length}</p>
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
